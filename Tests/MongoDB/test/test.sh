de86f2a30f70632e4dd0ee1bd1337efb3dddc269
Dmitriy Elsukov <demndik@yandex.ru>
2020-04-23 01:53:34 +0300

    update test start script

--- Tests/MongoDB/test/test.sh
+++ Tests/MongoDB/test/test.sh
@@ -2,6 +2,7 @@ echo "Preparing fixtures"
 rm -R ./temp/*
 echo "" > temp/dumb
 cp -R fixture/* temp/
+run git update-index --chmod=+x test.sh
 
 echo "Running tests"
 node ../../../main.js --no-splash --fall-on-errors
